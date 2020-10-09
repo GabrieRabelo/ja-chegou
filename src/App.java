47bff0d817edba4766e954f1aa97223073646859
gabrierabelo <gabriel.almeida97@gmail.com>
2020-10-09 20:34:06 -0300

    01/@gabrierabelo: Termina de implementar classe inicial do projeto

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -7,33 +8,43 @@ public class App {
         Scanner in = new Scanner(System.in);
         int opcao = 1;
 
-        //Operador opatual = new Operador (nome, id, ....)
-
-        System.out.println("App já chegou do Condomínio ES");
+        System.out.println("Bem vindo ao App já chegou do Condomínio ES");
         System.out.println("===============================");
-        System.out.println("");
-        //System.out.println("O usuário atual é: " + opatual);
-        System.out.println("");
-        System.out.println("Escolha uma das opções abaixo: ");
-        System.out.println("1- Mudar operador");
-        System.out.println("0- Sair");
 
-        opcao = in.nextInt();
+        while(opcao!= 0){       // Repetição do menu
 
-        switch (opcao){
-            case 1:
-                System.out.println("Digite as iniciais do novo operador: ");
-                String iniciaisNovoOperador = in.nextLine();
-                //opatual = ListaOperadores.mudarOperador(iniciaisNovoOperador);
-                break;
-            case 2:
-            case 3:
-            case 0:
-            default:
+            System.out.println();
+            System.out.println("Escolha uma das opções abaixo: ");
+            System.out.println("1- Testar método 1");
+            System.out.println("0- Sair");
 
+            try{       // Impede que usuário digite letra
+                opcao = in.nextInt();
+                in.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Opção inválida, tente novamente.");
+                in.nextLine();
+                continue;
+            }
+
+
+            switch (opcao){
+                case 1:
+                    metodoUm(); // Executa método 1
+                    break;
+                case 0:     // Sai do menu, encerra programa
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
         }
+    }
 
-
+    /**
+     * Exemplo de método chamado pelo menu
+     */
+    public static void metodoUm() {
+        System.out.println("Método 1 executado com sucesso");
     }
 
 }
