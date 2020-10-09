24c80622ec3c845728de531360209b8275dea319
Roberto Luís Rezende <robertodermato@gmail.com>
2020-10-09 19:23:17 -0300

    01/@robertodermato: Inicia desenvolvimento da classe App

--- src/App.java
+++ src/App.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int opcao = 1;
+
+        //Operador opatual = new Operador (nome, id, ....)
+
+        System.out.println("App já chegou do Condomínio ES");
+        System.out.println("===============================");
+        System.out.println("");
+        //System.out.println("O usuário atual é: " + opatual);
+        System.out.println("");
+        System.out.println("Escolha uma das opções abaixo: ");
+        System.out.println("1- Mudar operador");
+        System.out.println("0- Sair");
+
+        opcao = in.nextInt();
+
+        switch (opcao){
+            case 1:
+                System.out.println("Digite as iniciais do novo operador: ");
+                String iniciaisNovoOperador = in.nextLine();
+                //opatual = ListaOperadores.mudarOperador(iniciaisNovoOperador);
+                break;
+            case 2:
+            case 3:
+            case 0:
+            default:
+
+        }
+
+
+    }
+
 }
