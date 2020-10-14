025d07dd69d1377f9054c743185a2d3ea7ceedd1
Luiz Eduardo <luizdudumr@gmail.com>
2020-10-13 21:48:10 -0300

    Cria as classes Operador e Operadores - WIP

---
+++ src/Operador.java
@@ -0,0 +1,28 @@
+public class Operador{
+    private String name;
+    private int id;
+
+    // cName para construtorName
+    public Operador(String cName, int cId){
+        if(cId < 0){
+            throw new IllegalArgumentException("ID Inválido! Selecione um ID maior que zero");
+        }
+        this.name = cName;
+        this.id = cId;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setId(int newId){
+        this.id = newId;
+    }
+    public int getId(){
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Operador: " + name + " - ID: " + id;
+    }
+}
