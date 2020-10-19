79da5543bb6bad8a4de62844dff821f55a2afeb4
julianebea <julianebeatrycce@gmail.com>
2020-10-19 00:26:15 -0300

    09/ @Juliane: Adicionado o método buscaNaoRetirada() na classe ListaDeEntregas

--- src/ListaDeEntregas.java
+++ src/ListaDeEntregas.java
@@ -32,6 +32,15 @@ public class ListaDeEntregas {
         return subLista;
     }
 
+    public ArrayList<Entrega> buscaNaoRetirada(ArrayList<Entrega> todasEntregas){
+        ArrayList<Entrega> naoRetirada = new ArrayList<>();
+        for (Entrega entrega : todasEntregas){
+            if(entrega.getData() == null)
+                naoRetirada.add(entrega);
+        }
+        return naoRetirada;
+    }
+
     @Override
     public String toString() {
         String ret = "===== Lista de Entregas =====\n";
